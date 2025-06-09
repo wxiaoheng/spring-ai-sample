f119d3b409eb857676b6bbbc260965b577a2b9e2
wxiaoheng <40820522+wxiaoheng@users.noreply.github.com>
2025-06-09 18:36:25 +0800

    Update WordParser.java

--- rag-sample/src/main/java/com/wxh/ai/rag/split/WordParser.java
+++ rag-sample/src/main/java/com/wxh/ai/rag/split/WordParser.java
@@ -1,19 +1,3 @@
-/**
- * 文件描述
- *
- * @ProductName: Light Code
- * @ProjectName: spring-ai-sample
- * @Package: com.wxh.ai.rag.split
- * @Description: note
- * @Author: wangxh
- * @CreateDate: 2025/6/9 16:10
- * @UpdateUser: wangxh
- * @UpdateDate: 2025/6/9 16:10
- * @UpdateRemark: The modified content
- * @Version: 1.0
- * <p>
- * Copyright © 2025 Hundsun Technologies Inc. All Rights Reserved
- **/
 package com.wxh.ai.rag.split;
 
 import org.apache.poi.xwpf.usermodel.*;
